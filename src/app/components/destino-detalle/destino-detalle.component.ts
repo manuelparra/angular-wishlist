1c2b0c794b21df434be8ce23eec04fea15644c08
Manuel Parra <manuelparra@live.com.ar>
2019-10-29 14:36:39 -0400

    Inclimos inyección de dependencias nivel intermedio 2

--- src/app/components/destino-detalle/destino-detalle.component.ts
+++ src/app/components/destino-detalle/destino-detalle.component.ts
@@ -1,7 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, InjectionToken } from '@angular/core';
 import { DestinosApiClient } from './../../models/destinos-api-client.model';
 import { DestinoViaje } from './../../models/destino-viaje.model';
 import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.module';
+
+interface AppConfig {
+  apiEndpoint: String;
+}
+
+const APP_CONFIG_VALUE: AppConfig = {
+  apiEndpoint: 'mi_api.com'
+};
+
+const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
+
+class DestinosApiClientDecorated extends DestinosApiClient {
+  constructor(@Inject(APP_CONFIG) private config: AppConfig, store: Store<AppState>) {
+    super(store);
+  }
+  getById(id: String): DestinoViaje {
+    console.log('llamando por la clase decorada!');
+    console.log('config: ' + this.config.apiEndpoint);
+    return super.getById(id);
+  }
+}
 
 class DestinosApiClientViejo {
   getById(id: String): DestinoViaje {
@@ -15,9 +38,14 @@ class DestinosApiClientViejo {
   templateUrl: './destino-detalle.component.html',
   styleUrls: ['./destino-detalle.component.scss'], 
   providers: [
-    DestinosApiClient, 
-    { provide: DestinosApiClientViejo, useExisting: DestinosApiClient }
+    { provide: DestinosApiClient, useClass: DestinosApiClientDecorated },
+    { provide: DestinosApiClientViejo, useExisting: DestinosApiClient },
+    { provide: APP_CONFIG, useValue: APP_CONFIG_VALUE }
   ]
+  //providers: [
+  //  DestinosApiClient, 
+  //  { provide: DestinosApiClientViejo, useExisting: DestinosApiClient }
+  //]
   //providers: [DestinosApiClient]
   
 })
