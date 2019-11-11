b6c57ff3dafdfb73f7ed14310216a4dad8f65f3b
Manuel Parra <manuelparra@live.com.ar>
2019-11-11 12:24:08 -0400

    Include cypress test

---
+++ cypress/integration/examples/main.view.spec.js
@@ -0,0 +1,8 @@
+describe('ventana principal', () => {
+  it('tiene encabezado correcto y en español por defecto', () => {
+    cy.visit('http://localhost:4200');
+    cy.contains('angular-wishlist');
+    cy.get('h1 b').should('contain', 'HOLA es');
+    cy.get('b').should('contain', 'Venezuela');
+  });
+});
